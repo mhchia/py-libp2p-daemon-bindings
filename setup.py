b011a0697106de321d872987c548bf695a851f24
mhchia <kevin.mh.chia@gmail.com>
2018-11-22 22:47:17 +0800

    Add tox and setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name="p2pclient",
+    version="0.0.1",
+    packages=["p2pclient"],
+    install_requires=[
+        "base58",
+        "multiaddr",
+        "protobuf",
+        "pytest",
+    ],
+)
+

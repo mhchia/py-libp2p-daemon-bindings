d5c1208693ddb1063535437b63db8d0d5436ea21
mhchia <kevin.mh.chia@gmail.com>
2018-11-26 14:58:08 +0800

    Add p2pd fixture

--- tests/test_p2pclient.py
+++ tests/test_p2pclient.py
@@ -1,5 +1,9 @@
 import asyncio
+from collections import (
+    namedtuple,
+)
 import os
+import subprocess
 
 import pytest
 
@@ -55,29 +59,47 @@ def start_p2pd(control_path):
         os.unlink(control_path)
     except FileNotFoundError:
         pass
-    os.system("p2pd -sock={} -dht 2>&1 1>/tmp/p2pd_{}.log &".format(
-        control_path,
-        control_path[5:]
-    ))
+    return subprocess.Popen(
+        "p2pd -sock={} -dht 2>&1 1>/tmp/p2pd_{}.log".format(
+            control_path,
+            control_path[5:],
+        ),
+        shell=True,
+    )
+
+
+P2PDInfo = namedtuple('P2PDInfo', ['proc', 'control_path', 'listen_path'])
+
+
+@pytest.yield_fixture(scope="function")
+def make_p2pd():
+    p2pd_procs = {}
+    def _make_p2pd(number):
+        if number in p2pd_procs:
+            p2pd_info = p2pd_procs[number]
+            return p2pd_info.control_path, p2pd_info.listen_path
+        control_path = f"/tmp/test_p2pd_control_{number}"
+        listen_path = f"/tmp/test_p2pd_listen_path_{number}"
+        try:
+            os.unlink(listen_path)
+        except FileNotFoundError:
+            pass
+        proc = start_p2pd(control_path)
+        p2pd_procs[number] = P2PDInfo(proc, control_path, listen_path)
+        return control_path, listen_path
+
+    yield _make_p2pd
+
+    # teardown
+    for _, p2pd_info in p2pd_procs.items():
+        p2pd_info.proc.terminate()
 
 
 @pytest.mark.asyncio
-async def test_client_integration():
-    control_path_0 = "/tmp/test_p2pd_control_0"
-    listen_path_0 = "/tmp/test_p2pd_listen_path_0"
-    control_path_1 = "/tmp/test_p2pd_control_1"
-    listen_path_1 = "/tmp/test_p2pd_listen_path_1"
-    try:
-        os.unlink(listen_path_0)
-    except FileNotFoundError:
-        pass
-    try:
-        os.unlink(listen_path_1)
-    except FileNotFoundError:
-        pass
-    os.system("killall p2pd")
-    start_p2pd(control_path_0)
-    start_p2pd(control_path_1)
+async def test_client_integration(make_p2pd):
+    control_path_0, listen_path_0 = make_p2pd(0)
+    control_path_1, listen_path_1 = make_p2pd(1)
+
     await asyncio.sleep(2)
 
     c0 = Client(control_path_0, listen_path_0)
@@ -111,4 +133,12 @@ async def test_client_integration():
     writer.close()
     await asyncio.sleep(0.2)  # yield
 
-    await c0.find_peer(peer_id_1)
+    # test dht.find_peer
+    pinfo = await c0.find_peer(peer_id_1)
+    print(pinfo)
+
+
+def test_abc(make_p2pd):
+    control_path_1, listen_path_1 = make_p2pd(0)
+    pass
+

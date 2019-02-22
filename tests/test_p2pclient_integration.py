4dcfb163c821bddfb7b192e74981534144a6ddc7
mhchia <kevin.mh.chia@gmail.com>
2019-02-22 18:19:32 +0800

    flake8, no line break

--- tests/test_p2pclient_integration.py
+++ tests/test_p2pclient_integration.py
@@ -198,10 +198,8 @@ async def test_client_close(p2pds):
     await c0.close()
     assert c0.listener is None
     # test case: ensure there is no sockets after closing
-    assert (
-        listener.sockets is None or  # for versions before python 3.7
-        len(listener.socket) == 0  # for versions 3.7+
-    )
+    # for versions before python 3.7 and 3.7+, respectively
+    assert listener.sockets is None or len(listener.socket) == 0
     # test case: it's fine to listen again, after closing
     await c0.listen()
 

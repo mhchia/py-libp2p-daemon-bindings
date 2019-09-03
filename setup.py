0017625e24cbb23246fb6017fed659a01bc13945
mhchia <kevin.mh.chia@gmail.com>
2019-09-03 18:43:10 +0800

    Update deps

--- setup.py
+++ setup.py
@@ -11,13 +11,23 @@ deps = {
     "libp2p": [
         "base58>=1.0.3",
         "multiaddr>=0.0.8,<0.1.0",
-        "protobuf==3.9.0",
+        "protobuf>=3.9.0",
         "pymultihash>=0.8.2",
         "libp2p @ git+https://git@github.com/mhchia/py-libp2p@dfd9ebd",
     ],
-    "test": ["pytest", "pytest-asyncio", "pytest-cov"],
-    "lint": ["flake8", "mypy", "black", "mypy-protobuf", "isort"],
-    "dev": ["tox"],
+    "test": [
+        "pytest>=4.6.3,<5.0.0",
+        "pytest-asyncio>=0.10.0,<1.0.0",
+        "pytest-cov>=2.7.1,<3.0.0",
+    ],
+    "lint": [
+        "mypy>=0.701,<1.0",
+        "mypy-protobuf>=1.15",
+        "black>=19.3b0",
+        "isort>=4.3.21",
+        "flake8>=3.7.7,<4.0.0",
+    ],
+    "dev": ["tox>=3.13.2,<4.0.0"],
 }
 
 deps["dev"] = deps["libp2p"] + deps["test"] + deps["lint"] + deps["dev"]
